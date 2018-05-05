c56f8a30f1b801c97c23a7ac222934f0114d20b0
Kiptim <kiptim54@gmail.com>
2018-05-06 02:05:38 +0300

    change debug mode

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Gallery.urls'
