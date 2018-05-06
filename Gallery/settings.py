9234bd46b763425257ab843308ba8370b3ceb366
Kiptim <kiptim54@gmail.com>
2018-05-06 10:06:58 +0300

    add remove

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -137,4 +137,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = "/media/articles/"
